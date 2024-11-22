a267e30a56f14b7122b217e30dfba423aa7043b3
Niroxapex1 <taherinima28@gmail.com>
2024-11-23 00:31:27 +0330

    Update pathmaker.py

--- Funcs/pathmaker.py
+++ Funcs/pathmaker.py
@@ -1,4 +1,5 @@
 import sys,os
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if project_path not in sys.path:
-    sys.path.append(project_path)
+def pathmaker(path='..'):
+    project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), path))
+    if project_path not in sys.path:
+        return sys.path.append(project_path)
